52632c698aae1effc12619cb3fe228d0cde3b23a
wikitopian <wikitopian@pm.me>
2026-02-23 15:51:45 -0500

    build: #0 gemini

--- src/merman.js
+++ src/merman.js
@@ -1,5 +1,16 @@
-export default class MermanUI {
-  constructor() {
-    // Empty class for now
+import OTM from 'on_the_money';
+
+export default class MermanUI extends OTM {
+  constructor(elementId = 'merman-main') {
+    super(elementId);
+    this.state = {
+      viewMode: 'edit',
+      currentFile: null
+    };
+  }
+
+  // OTM methods (render, onStateChange, etc.)
+  render() {
+    return '<h1>Merman</h1>';
   }
 }
--- src/merman.test.js
+++ src/merman.test.js
@@ -2,14 +2,16 @@ import test from 'node:test';
 import assert from 'node:assert';
 import MermanUI from './merman.js';
 
-test('MermanUI class tests', async (t) => {
-  await t.test('it should be instantiatable', () => {
-    const instance = new MermanUI();
+test('MermanUI class tests (OTM Instrumented)', async (t) => {
+  await t.test('it should be instantiatable as an OTM class', () => {
+    // We provide a mock elementId since we're in a Node test environment
+    const instance = new MermanUI('mock-element');
     assert.ok(instance instanceof MermanUI);
   });
 
-  await t.test('it should have a render method (unimplemented)', () => {
-    const instance = new MermanUI();
+  await t.test('it should have an OTM render method returning content', () => {
+    const instance = new MermanUI('mock-element');
     assert.strictEqual(typeof instance.render, 'function', 'Method render should exist');
+    assert.ok(instance.render().includes('Merman'));
   });
 });
